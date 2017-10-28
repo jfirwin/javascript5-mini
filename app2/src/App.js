e855276fad59964241e1ad0b0544142c364fbfb5
Foster Irwin <jfosterirwin@Fosters-MacBook-Air.local>
2017-10-28 12:05:18 -0600

    renamed app_2 to app2 and competed debugging

--- app2/src/App.js
+++ app2/src/App.js
@@ -4,15 +4,14 @@ import './App.css';
 import FoodItem from './FoodItem';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       favoriteFoods: ['lettuce', 'carrots', 'beets', 'beans', 'air']
     }
   }
   render() {
-
-    const favFoods = this.state.favoriteFoods.map( item, i => {
+    const favFoods = this.state.favoriteFoods.map( (item, i) => {
         return <FoodItem foodItem={item}/>
     })
 
