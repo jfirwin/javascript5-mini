94bc19ec8250cf01d296553e28a9e455b25bb6ba
Foster Irwin <jfosterirwin@Fosters-MacBook-Air.local>
2017-10-28 12:05:47 -0600

    completed app4 debugging

--- app4/src/App.js
+++ app4/src/App.js
@@ -10,11 +10,11 @@ class App extends Component {
     }
   }
 
-  getCars() {
+  getCars = () => {
     axios.get('https://joes-autos.herokuapp.com/api/vehicles')
     .then(res => {
       this.setState({
-        cars: res
+        cars: res.data
       })
     })
   }
@@ -23,7 +23,9 @@ class App extends Component {
     return (
       <div className="App">
         <button onClick={this.getCars}>Get cars</button>
-        {this.state.cars}
+        {this.state.cars.map((car) => {
+          return <p>{car.make} {car.model}</p>
+        })}
       </div>
     );
   }
